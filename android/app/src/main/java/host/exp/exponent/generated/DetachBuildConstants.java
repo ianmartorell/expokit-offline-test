a67e7f2e4ddf4905a243d2f2e60ea7b0094acc8a
Ian Martorell <ianmartorell@gmail.com>
2018-10-17 13:44:23 +0200

    Add signing config

--- android/app/src/main/java/host/exp/exponent/generated/DetachBuildConstants.java
+++ android/app/src/main/java/host/exp/exponent/generated/DetachBuildConstants.java
@@ -3,6 +3,6 @@ package host.exp.exponent.generated;
 // This file is auto-generated. Please don't rename!
 public class DetachBuildConstants {
 
-  public static final String DEVELOPMENT_URL = "expb975a05231d24e64aaf75b8618a2c295://localhost:19000";
+  public static final String DEVELOPMENT_URL = "expb975a05231d24e64aaf75b8618a2c295://localhost:80";
 
 }
