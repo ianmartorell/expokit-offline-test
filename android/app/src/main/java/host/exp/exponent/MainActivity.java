5e8810b1b381eb8a4729d556f5f1e24d300e2e0b
Ian Martorell <ianmartorell@gmail.com>
2018-11-24 16:56:44 +0100

    Publish app

--- android/app/src/main/java/host/exp/exponent/MainActivity.java
+++ android/app/src/main/java/host/exp/exponent/MainActivity.java
@@ -17,7 +17,7 @@ public class MainActivity extends DetachActivity {
 
   @Override
   public String publishedUrl() {
-    return "exp://exp.host/@waitless/offline-test";
+    return "exp://exp.host/@ianmartorell/offline-test";
   }
 
   @Override
--- android/app/src/main/java/host/exp/exponent/generated/AppConstants.java
+++ android/app/src/main/java/host/exp/exponent/generated/AppConstants.java
@@ -26,10 +26,18 @@ public class AppConstants {
     List<Constants.EmbeddedResponse> embeddedResponses = new ArrayList<>();
 
     
+        
+        
+        
+        
+        
+        
+        
+        
         // ADD EMBEDDED RESPONSES HERE
         // START EMBEDDED RESPONSES
-        embeddedResponses.add(new Constants.EmbeddedResponse("https://exp.host/@ianmartorell/offline-test/index.exp", "assets://shell-app-manifest.json", "application/json"));
-        embeddedResponses.add(new Constants.EmbeddedResponse("https://d1wp6m56sqw74a.cloudfront.net/%40ianmartorell%2Foffline-test%2F1.0.0%2F436656f4138a180f3a00873aa3f9e84b-30.0.0-android.js", "assets://shell-app.bundle", "application/javascript"));
+        embeddedResponses.add(new Constants.EmbeddedResponse("https://exp.host/@ianmartorell/offline-test", "assets://shell-app-manifest.json", "application/json"));
+        embeddedResponses.add(new Constants.EmbeddedResponse("https://d1wp6m56sqw74a.cloudfront.net/%40ianmartorell%2Foffline-test%2F1.1.0%2F737d804433b48f50c1e87f144b949b35-30.0.0-android.js", "assets://shell-app.bundle", "application/javascript"));
         // END EMBEDDED RESPONSES
     EMBEDDED_RESPONSES = embeddedResponses;
   }
